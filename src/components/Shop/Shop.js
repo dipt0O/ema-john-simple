cc9a206da4d10a01a95894cf44ff991e45751caf
dipt0o <6sabit@gmail.com>
2021-02-26 01:17:43 +0600

    fixed

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import "./Shop.css";
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
-    const [products] = useState(first10);
+    const [products, setProducts] = useState(first10);
     return (
         <div className="shop-container">
           <div className="product-container">
