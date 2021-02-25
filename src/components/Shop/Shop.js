cf832659af341f72958ed9f2af6b26037eb1617b
dipt0o <6sabit@gmail.com>
2021-02-26 01:08:30 +0600

    area-created

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,18 +1,24 @@
 import  React  from 'react';
 import fakeData from '../../fakeData';
 import { useState } from 'react';
+import "./Shop.css";
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products] = useState(first10);
     return (
-        <div>
-           <h1>this is shop</h1>
-           <h3>{products.length}</h3> 
-           <ul>
-           {
-               products.map(product =><li>{product.name}</li>)
-           }
+        <div className="shop-container">
+          <div className="product-container">
+            <ul>
+                {
+                  products.map(product =><li>{product.name}</li>)
+                }    
            </ul>
+           </div>
+           <div className="cart-container">
+               <h3>This is cart</h3>
+           </div>
+          
+           
 
         </div>
     );
