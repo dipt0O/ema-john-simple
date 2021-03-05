a0ea2dc52bcabfc254202774ae0d0771ac85033c
dipt0o <6sabit@gmail.com>
2021-03-06 04:33:42 +0600

    debug-done

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,30 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
+import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
-    const[cart, setCart]=useState([]);
-    useEffect(()=>{
-    // cart
-    const savedCart = getDatabaseCart();
-    const productKeys = Object.keys(savedCart);
+    const [cart, setCart] = useState([]);
 
-    const cartProducts = productKeys.map(key => {
-        const product = fakeData.find(pd => pd.key === key);
-        product.quantity = savedCart[key];
-        return product;
-    });
-    setCart(cartProducts)
+    const removeProduct = (productKey) => {
+        // console.log('remove-clicked', productKey);
+
+        const newCart = cart.filter(pd => pd.key !== productKey)
+        setCart(newCart);
+        removeFromDatabaseCart(productKey);
+    }
+    useEffect(() => {
+        // cart
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+
+        const cartProducts = productKeys.map(key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        });
+        setCart(cartProducts)
     }, [])
     return (
-        <div>
-           <h1>Cart Items: {cart.length}</h1>
-           {
-                cart.map(pd => <ReviewItem
-                    key={pd.key}
-                     product={pd}></ReviewItem>)
-           }
+        <div className="twin-container">
+            <div className="product-container">
+                {
+                    cart.map(pd => <ReviewItem
+                        key={pd.key}
+                        removeProduct={removeProduct}
+                        product={pd}></ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
 
         </div>
     );
